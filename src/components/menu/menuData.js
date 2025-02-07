f030900363361717f945b8222271d6bc353bc2ed
Pavel <zenko_pavel@mail.ru>
2025-02-07 23:40:36 +0300

    fix: image paths have been adjusted, package.json has been edited

--- src/components/menu/menuData.js
+++ src/components/menu/menuData.js
@@ -5,42 +5,42 @@ export const menuData = {
             name: 'Cheeseburger With Salad',
             price: '18.00',
             rating: 5,
-            img: '/images/burger1.png'
+            img: 'https://kkkiska.github.io/efood/images/burger1.png'
         },
         {
             id: 1,
             name: 'Beef Burger',
             price: '15.00',
             rating: 4.5,
-            img: '/images/burger2.png'
+            img: 'https://kkkiska.github.io/efood/images/burger2.png'
         },
         {
             id: 2,
             name: 'Royel Cheeseburger',
             price: '16.00',
             rating: 4,
-            img: '/images/burger3.png'
+            img: 'https://kkkiska.github.io/efood/images/burger3.png'
         },
         {
             id: 3,
             name: 'Black Gambugrer ',
             price: '14.00',
             rating: 5,
-            img: '/images/burger4.png'
+            img: 'https://kkkiska.github.io/efood/images/burger4.png'
         },
         {
             id: 4,
             name: 'Chicken Burger',
             price: '15.00',
             rating: 5,
-            img: '/images/burger5.png'
+            img: 'https://kkkiska.github.io/efood/images/burger5.png'
         },
         {
             id: 5,
             name: 'Tasty Burger',
             price: '20.00',
             rating: 4,
-            img: '/images/burger6.png'
+            img: 'https://kkkiska.github.io/efood/images/burger6.png'
         },
     ],
     Pizza: [
@@ -48,19 +48,19 @@ export const menuData = {
             name: 'Mushroom pizza',
             price: '18.00',
             rating: 5,
-            img: '/images/pizza1.png'
+            img: 'https://kkkiska.github.io/efood/images/pizza1.png'
         },
         {
             name: 'Greece pizza',
             price: '17.00',
             rating: 4.5,
-            img: '/images/pizza2.png'
+            img: 'https://kkkiska.github.io/efood/images/pizza2.png'
         },
         {
             name: 'Dou pizza',
             price: '19.00',
             rating: 4.5,
-            img: '/images/pizza3.png'
+            img: 'https://kkkiska.github.io/efood/images/pizza3.png'
         }
     ],
 } 
